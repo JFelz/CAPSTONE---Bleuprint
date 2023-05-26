495db4ae7050c0ce653dd085e0be594de17775cd
Jovanni Feliz <kfeliz@c.ringling.edu>
2023-05-25 21:22:08 -0500

    setup

---
+++ components/digitalAssetCard.js
@@ -0,0 +1,5 @@
+// import React from 'react';
+// import PropTypes from 'prop-types';
+// import { Button } from 'react-bootstrap';
+// import { Card } from 'react-bootstrap';
+// import Link from 'next/link';
--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,37 @@
-// import { Button } from 'react-bootstrap'; // TODO: COMMENT IN FOR AUTH
-// import { signOut } from '../utils/auth'; // TODO: COMMENT IN FOR AUTH
-// import { useAuth } from '../utils/context/authContext'; // TODO: COMMENT IN FOR AUTH
+// // import { Button } from 'react-bootstrap'; // TODO: COMMENT IN FOR AUTH
+// // import { signOut } from '../utils/auth'; // TODO: COMMENT IN FOR AUTH
+// // import { useAuth } from '../utils/context/authContext'; // TODO: COMMENT IN FOR AUTH
+// import { useState, useEffect } from 'react';
+// import getDigitalAssets from '../api/digitalAssets';
+// // import { Card } from 'react-bootstrap';
 
-function Home() {
-  // const { user } = useAuth(); // TODO: COMMENT IN FOR AUTH
+// function Home() {
+//   // const { user } = useAuth(); // TODO: COMMENT IN FOR AUTH
+//   // const [video, setVideos] = useState();
 
-  const user = { displayName: 'Dr. T' }; // TODO: COMMENT OUT FOR AUTH
-  return (
-    <div
-      className="text-center d-flex flex-column justify-content-center align-content-center"
-      style={{
-        height: '90vh',
-        padding: '30px',
-        maxWidth: '400px',
-        margin: '0 auto',
-      }}
-    >
-      <h1>Hello {user.displayName}! </h1>
-    </div>
-  );
-}
+//   const seePlaylistVideos = () => {
+//     getDigitalAssets().then(setVideos);
+//   };
 
-export default Home;
+//   useEffect(() => {
+//     seePlaylistVideos();
+//   }, []);
+//   // const user = { displayName: 'Jovanni' }; // TODO: COMMENT OUT FOR AUTH
+//   return (
+//     <div
+//       className="text-center d-flex flex-column justify-content-center align-content-center"
+//       style={{
+//         height: '90vh',
+//         padding: '30px',
+//         maxWidth: '400px',
+//         margin: '0 auto',
+//       }}
+//     >
+
+//       {/* <h1>Hello {user.displayName}! </h1> */}
+//       {/* <Card key={video} /> */}
+//     </div>
+//   );
+// }
+
+// export default Home;
