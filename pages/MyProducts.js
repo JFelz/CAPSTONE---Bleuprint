4ef61075be062ecb6c7d4049328b3c05fe3dc7a4
Jovanni Feliz <kfeliz@c.ringling.edu>
2023-06-05 00:08:32 -0500

    added userName on products based off other accounts/Create phase of CRUD/CreateForm

--- pages/MyProducts.js
+++ pages/MyProducts.js
@@ -1,5 +1,20 @@
+import Link from 'next/link';
 import React from 'react';
+import { Button, Container } from 'react-bootstrap';
 
 export default function ManageProducts() {
-  return <div>My Products</div>;
+  return (
+    <>
+      <Container style={{ display: 'flex', justifyContent: 'center', marginTop: '50px' }}>
+        <Link href="/Manageproducts/new" passHref>
+          <Button style={{ backgroundColor: '#35CEB3', borderColor: '#35CEB3' }}> Add a Product </Button>
+        </Link>
+      </Container>
+      <Container>
+        <div>
+          <h1> hi </h1>
+        </div>
+      </Container>
+    </>
+  );
 }
